45a11e3253d9193821eb520a6a2db1ee1792abcc
Jaime Arrocha <jarr@innercoder.com>
2015-12-17 21:06:36 -0600

    Small code optimization

--- tcp3_pscannerV2/tcp3_pscannerv2.c
+++ tcp3_pscannerV2/tcp3_pscannerv2.c
@@ -1,7 +1,8 @@
 /*
  * This program uses TCP 3-way handshake to check if a port is open.
  * IPv4/6 compatible.
- * Not a very stealthy approach, only for API practice.
+ * Notes:
+ *	-Not a very stealthy approach, only for API practice.
  */
 
 #include <stdio.h>
@@ -13,7 +14,7 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-/* #define MAX 100 */
+#define DATA 100 
 
 void usage(char *name)
 {
@@ -26,13 +27,11 @@ void usage(char *name)
 int main (int argc, char **argv)
 {
 	int sockfd;
-	struct sockaddr_in client_addr;
-	struct sockaddr_in *sinp;
+	struct sockaddr_in *client_addr;
 	struct addrinfo hint;
 	struct addrinfo *result, *rp;
-	char buff[100];
-	int counter1, result1;
-	char *addrp;
+	char buff[DATA];
+	int counter1, result1, result2;
 	
 	if ((argc < 2) || (argc > 2))
 		usage(argv[0]);
@@ -41,7 +40,7 @@ int main (int argc, char **argv)
 	memset(&client_addr, 0, sizeof(struct sockaddr_in));
 
 	/* initializing addrinfo structure list */
-	hint.ai_flags = AI_CANONNAME;
+	hint.ai_flags = AI_ALL;
 	hint.ai_family = AF_UNSPEC;
 	hint.ai_socktype = SOCK_STREAM;
 	hint.ai_protocol = 0;
@@ -51,11 +50,13 @@ int main (int argc, char **argv)
 	hint.ai_next = NULL;
 
 	/* Converting the input address to network presentation */
-	result1 = inet_pton(AF_INET, argv[1], &client_addr.sin_addr);
+	result1 = inet_pton(AF_INET, argv[1], &client_addr->sin_addr);
 	if(result1 < 0 )
+		//result2 =inet_pton(AF_INET6, argv[1],
 		perror("Error converting IP to hostname.\n");
 	else
 		/* try convert input to IP */
+		//printf("
 		if(getaddrinfo(argv[1], 0, &hint, &result) != 0)
 			printf("Error getaddrinfo()\n");
 	
@@ -63,11 +64,13 @@ int main (int argc, char **argv)
 	for (rp = result; rp != NULL; rp = rp->ai_next) {
 		if (rp->ai_family == AF_INET) {
 			printf("IPV4 detected\n");
-			sinp = (struct sockaddr_in *) rp->ai_addr;
-			addrp = inet_ntop(AF_INET, &sinp->sin_addr, buff, 100);
-			printf("Address %s\n", addrp?addrp:"unknow");
+			client_addr = (struct sockaddr_in *) rp->ai_addr;
+			inet_ntop(AF_INET, &client_addr->sin_addr, buff, DATA);
+			printf("Address %s\n", buff);
 		} else if (rp->ai_family == AF_INET6)
-		       printf("IPV6 detected\n");	
+		       printf("IPV6 detected\n");
+
+
 		
 	}
 		//if ((sockfd = socket(rp->ai_family, SOCK_STREAM, 0)) == -1)
