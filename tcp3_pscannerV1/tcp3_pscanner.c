577797c4c455d952f55bde495114ddaa8eb1c406
Jaime Arrocha <jarr@innercoder.com>
2015-12-17 19:27:53 -0600

    Several minor changes

--- tcp3_pscannerV1/tcp3_pscanner.c
+++ tcp3_pscannerV1/tcp3_pscanner.c
@@ -1,5 +1,5 @@
 /*
- * This program uses TCP 3-way handshake to check if a port is open
+ * This program uses TCP 3-way handshake to check if a port is open.
  */
 
 #include <stdio.h>
@@ -10,7 +10,6 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-#define MAX 100
 
 void usage(char *name)
 {
@@ -22,34 +21,31 @@ void usage(char *name)
 int main (int argc, char **argv)
 {
 	int sockfd;
-	struct sockaddr_in client_addr;
-	//socketlen_t sin_size;
-	//char buffer[100];
+	struct sockaddr_in cl_addr;
 	int counter;
 	
 	if ((argc < 2) || (argc > 2))
 		usage(argv[0]);
 	
-	memset(&client_addr, 0, sizeof(struct sockaddr_in));
+	memset(&(cl_addr.sin_zero), 0, sizeof(cl_addr.sin_zero));
 	/* Specifying the address family */
-	client_addr.sin_family = AF_INET;
+	cl_addr.sin_family = AF_INET;
 	
 	/* Converting the input address to network presentation */
-	if(inet_pton(AF_INET, argv[1], &client_addr.sin_addr))
+	if(inet_pton(AF_INET, argv[1], &cl_addr.sin_addr))
 			printf("pton conversion successful\n");
 
 	for (counter = 0; counter < 65536; counter++) {	
 		/* Setting the port number to scan */
-		client_addr.sin_port = htons(counter);
+		cl_addr.sin_port = htons(counter);
 
 		if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 			perror("Error in socket()");
-		if((connect(sockfd, (struct sockaddr *) &client_addr, 
+		if((connect(sockfd, (struct sockaddr *) &cl_addr, 
 			sizeof(struct sockaddr_in)) == 0))
-			printf("Port %d is open\n", counter);
+			printf("Port[%d]: open\n", counter);
 		else
 			fflush(stdout);
-		/* printf("Port %d is close\n", client_addr.sin_port); */
 
 		close(sockfd);
 	}
