65edff7e2ecb2a17c08dfa2f83ce1ad1f1885288
Jaime Arrocha <jarr@innercoder.com>
2015-12-17 18:44:51 -0600

    checkpatch corrections

--- simple_server/simple_server.c
+++ simple_server/simple_server.c
@@ -15,7 +15,7 @@
 
 void dump(char *, const unsigned int);
 
-int main (void)
+int main(void)
 {
 	int sockfd, new_sockfd;
 	struct sockaddr_in host_addr, client_addr;
@@ -25,10 +25,10 @@ int main (void)
 	sin_size = sizeof(struct sockaddr_in);
 
 
-	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 		perror("in socket");
 
-	if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int))
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int))
 			== -1)
 		perror("setting socket option SO_REUSEADDR");
 
@@ -36,25 +36,25 @@ int main (void)
 	host_addr.sin_port = htons(S_PORT);	/* port, network byte order */
 	host_addr.sin_addr.s_addr = 0;		/* use host address */
 	/* zero the rest of struct */
-	memset(&(host_addr.sin_zero), '\0', sizeof(host_addr.sin_zero));	
+	memset(&(host_addr.sin_zero), '\0', sizeof(host_addr.sin_zero));
 
-	if(bind(sockfd, (struct sockaddr *)&host_addr, sizeof(struct sockaddr))
+	if (bind(sockfd, (struct sockaddr *)&host_addr, sizeof(struct sockaddr))
 			== -1)
 		perror("binding to socket");
-	if(listen(sockfd, 5) == -1)
+	if (listen(sockfd, 5) == -1)
 		perror("listening on socket");
 
-	while(1) {
+	while (1) {
 		new_sockfd = accept(sockfd, (struct sockaddr *) &client_addr,
 				 &sin_size);
-		if(new_sockfd == -1)
+		if (new_sockfd == -1)
 			perror("accepting connection");
 		printf("server: got connection from %s port %d\n",
-			inet_ntoa(client_addr.sin_addr), 
+			inet_ntoa(client_addr.sin_addr),
 			ntohs(client_addr.sin_port));
 		recv_length = recv(new_sockfd, &buffer, 1024, 0);
 
-		while(recv_length > 0) {
+		while (recv_length > 0) {
 			printf("RECV: %d bytes\n", recv_length);
 			dump(buffer, recv_length);
 			recv_length = recv(new_sockfd, &buffer, 1024, 0);
@@ -65,33 +65,31 @@ int main (void)
 	return 0;
 }
 
-// dumps raw memory in hex byte and printable split format
-void dump(char *data_buffer, const unsigned int length) 
+/* dumps raw memory in hex byte and printable split format */
+void dump(char *data_buffer, const unsigned int length)
 {
 	unsigned char byte;
 	unsigned int i, j;
 	
-	for(i=0; i < length; i++) {
+	for (i = 0; i < length; i++) {
 		byte = data_buffer[i];
 		printf("%02x ", data_buffer[i]);  /* displays byte in hex */
 		
-		if(((i % 16) == 15) || (i == length - 1)) {
-			for(j = 0; j < 15 -(i % 16); j++)
+		if (((i % 16) == 15) || (i == length - 1)) {
+			for (j = 0; j < 15 - (i % 16); j++)
 				printf("   ");
-			
 			printf("| ");
 			/* display printable bytes from line */
-			for(j = (i -(i % 16)); j <= i; j++) {  
+			for (j = (i - (i % 16)); j <= i; j++) {
 				byte = data_buffer[j];
-				
 				/* outside printable char range */
-				if((byte > 31) && (byte < 127)) 
+				if ((byte > 31) && (byte < 127))
 					printf("%c", byte);
 				else
 					printf(".");
 			}
 			/* end of the dump line (each line 16 bytes) */
-			printf("\n"); 
+			printf("\n");
 		}
 	}
 }
